dcc323534e0062fddf9b4871cde4d7eab3676f4c
Louise Au Yeung <louise@eabx.org>
2013-09-12 14:37:59 -0400

    passed tests for visitor submitting contact form to doghub

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Doghub::Application.routes.draw do
+  resources :responses
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'responses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +42,7 @@ Doghub::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
